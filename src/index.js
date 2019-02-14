5d03c181e7d87e4daa5c054cc0f79a4fed58d0e1
Chi Ho <>
2019-02-14 22:01:16 +0100

    combine reducers added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-<<<<<<< HEAD
-=======
-import rootReducer from './reducers';
-import * as serviceWorker from './serviceWorker';
-import Todo from './components/App';
-
->>>>>>> 182824a66c8e669674854a8dc74d6dc701777e2f
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './reducer';
